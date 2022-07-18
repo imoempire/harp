46f8202635e793b7f96fd87181139278b47ac36e
imoempire <mrimoempire@gmail.com>
2022-07-18 11:42:16 +0000

    rendeing app.js

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MyStack from './src/Routes/Stacks';
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MyStack/>
   );
 }
 
