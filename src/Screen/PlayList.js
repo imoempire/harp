7aa67baf21b651befaa6802b3ec7843dc4baec71
imoempire <mrimoempire@gmail.com>
2022-07-18 11:40:05 +0000

    create play list screen & details

---
+++ src/Screen/PlayList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const PlayList = () => {
+   return (
+      <View>
+         <Text>PlayList</Text>
+      </View>
+   );
+}
+
+const styles = StyleSheet.create({})
+
+export default PlayList;
---
+++ src/Screen/PlayListDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const PlayListDetails = () => {
+   return (
+      <View>
+         <Text>PlayListDetails</Text>
+      </View>
+   );
+}
+
+const styles = StyleSheet.create({})
+
+export default PlayListDetails;
